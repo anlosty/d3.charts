2b6d3fdacf8ba6fffbf2dc695745548e726c8db2
anlosty <anlosty@gmail.com>
2012-12-26 15:27:08 -0800

    replace strings with rect on circle

--- circle.js
+++ circle.js
@@ -10,27 +10,22 @@ svg.attr("height", svgHeight);
 
 var g = svg.append("g");
 
-var circle = cx="50" cy="50" r="50";
-
-
-function drawRect (column, row) {
+function drawCircle (x, y, radius) {
 	var startPadding = 30;
 	var columnPadding = 30;
 	var columnWidth = 20;
 	var yStep = 0.5;
 	var height = row * yStep;
 	var x = (column - 1) * (columnWidth + columnPadding) + startPadding;
-	var rect = g.append("rect");
-	rect.attr("width", columnWidth);
-	rect.attr("height", height);
-	rect.attr("x", x);
-	rect.attr("y", svgHeight - height);
+	var circle = g.append("circle");
+	circle.attr("radius", 50);
+	circle.attr("x", 30);
+	circle.attr("y", 30);
 }
 
 // column - number of class in school
 // row - count of missed classes
 
-drawRect(1, 80);
-drawRect(2, 100);
-drawRect(3, 80);
-drawRect(4, 100);
+drawCircle(10, 30, 5);
+drawCircle(20, 30, 5);
+drawCircle(40, 30, 10);
