e4878d7e9692ce94ae91c635a346a1e5cd281c3b
anlosty <anlosty@gmail.com>
2012-12-25 16:25:54 -0800

    function for drawing bars

--- bars.js
+++ bars.js
@@ -1,4 +1,33 @@
+function half (val) {
+	return val / 2;
+}
+var svgWidth = 1000;
+var svgHeight = 300;
 var body = d3.select("body");
 var svg = body.append("svg");
-svg.attr("width", 1000);
-svg.attr("height", 500);
+svg.attr("width", svgWidth);
+svg.attr("height", svgHeight);
+
+var g = svg.append("g");
+
+function drawRect (column, row) {
+	var startPadding = 30;
+	var columnPadding = 30;
+	var columnWidth = 20;
+	var yStep = 0.5;
+	var height = row * yStep;
+	var x = (column - 1) * (columnWidth + columnPadding) + startPadding;
+	var rect = g.append("rect");
+	rect.attr("width", columnWidth);
+	rect.attr("height", height);
+	rect.attr("x", x);
+	rect.attr("y", svgHeight - height);
+}
+
+// column - number of class in school
+// row - count of missed classes
+
+drawRect(1, 80);
+drawRect(2, 100);
+drawRect(3, 80);
+drawRect(4, 100);
